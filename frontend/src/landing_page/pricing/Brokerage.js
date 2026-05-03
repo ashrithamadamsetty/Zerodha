4c24425d6b1f0ac9a0a59cda490886c2ae5465fb
Ashritha <ashritha683@gmail.com>
2026-05-03 18:33:58 +0530

    Fix: Remove unused imports and invalid href to resolve ESLint errors on Vercel

--- frontend/src/landing_page/pricing/Brokerage.js
+++ frontend/src/landing_page/pricing/Brokerage.js
@@ -4,7 +4,7 @@ function Brokerage() {
           <div className="container"> 
       <div className="row p-5 mt-5 border-bottom text-center" >
         <div className="col-8 p-4 text-center">
-            <a href=""><h3>Brokerage Calculator</h3></a>
+            <a href="https://example.com"><h3>Brokerage Calculator</h3></a>
         </div>
         <div className="col-4 p-4 text-center">
             <h3>List of charges</h3>
